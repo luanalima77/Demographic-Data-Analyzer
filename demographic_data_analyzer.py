32e5b65235f5edf13883cd9c6bab89df46b60687
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-08-22 09:34:32 -0300

    feat: adicionar cálculo da porcentagem de pessoas que possuem e que não possuem ensino superior com base no dataset adult.data.csv

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -19,8 +19,8 @@ def calculate_demographic_data(print_data=True):
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    higher_education = df.loc[(df['education'] == 'Bachelors') | (df['education'] == 'Masters') | (df['education'] == 'Doctorate')]
+    lower_education = df.loc[(df['education'] != 'Bachelors') & (df['education'] != 'Masters') & (df['education'] != 'Doctorate')]
 
     # percentage with salary >50K
     higher_education_rich = None
