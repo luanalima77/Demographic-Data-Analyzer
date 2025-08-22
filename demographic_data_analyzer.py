c8f4e66c4f0b4b9c90530a3dbcc13e2714fda1e4
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-08-22 09:57:25 -0300

    feat: adicionar cálculo para identificar ocupação mais popular entre quem ganha >50K na Índia

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -39,7 +39,7 @@ def calculate_demographic_data(print_data=True):
     highest_earning_country_percentage =  round((df.loc[df['salary'] == '>50K', 'native-country'].astype(str).value_counts()/df['native-country'].astype(str).value_counts() *100), 1).loc[highest_earning_country]
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = df.loc[(df['native-country'] == "India") & (df['salary'] == ">50K"), 'occupation'].value_counts().idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
